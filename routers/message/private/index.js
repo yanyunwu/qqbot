eccbc227c6cfae93e2d103c2c01368eb9584c3c4
yanyunwu <2480721346@qq.com>
2021-09-04 14:26:34 +0800

    私聊

--- routers/message/private/index.js
+++ routers/message/private/index.js
@@ -2,11 +2,7 @@
 const app = require('./create');
 
 // 配置接口模块
-require('./admin');
-require('./api');
 require('./master');
-require('./menu');
-require('./user');
 
 //导出接口模块
 module.exports = app;
