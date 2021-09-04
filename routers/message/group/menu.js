2c9f120a12f7224f598e154531eff6bc617713b8
yanyunwu <2480721346@qq.com>
2021-09-04 13:58:54 +0800

    测试

--- routers/message/group/menu.js
+++ routers/message/group/menu.js
@@ -1,7 +1,7 @@
 const app = require('./create');
 
 app.message('菜单', (event, bot) => {
-    let list = `           菜单6           
+    let list = `           菜单           
 主人指令  管理指令
 用户指令  其他指令
 娱乐功能  实用功能`;
