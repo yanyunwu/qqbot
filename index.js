bad75c47c7b61fb00b2e1003c8380c08d0bbc87f
yanyun <2480721346@qq.com>
2022-04-16 16:05:42 +0800

    初始化

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const frameBot = require('frame-bot');
 
 const bot = frameBot.createBot(2770315275);
-module.exports = bot;
+// module.exports = bot;
 const app = require('./routers/message/group');
 
 bot.use('message.group', app);
